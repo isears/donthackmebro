9f3b472f37db7b138982fb2612aebb5cbd9779b4
Isaac Sears <isaac.j.sears@gmail.com>
2018-09-02 15:27:54 -0400

    Make peda install idempotent

--- provision.sh
+++ provision.sh
@@ -4,13 +4,15 @@
 apt-get update
 apt-get -y upgrade
 apt-get -y install vim build-essential gcc-multilib g++-multilib gdb python3-pip \
-    default-jdk openvpn git tmux linux-headers-$(uname -r)
+    default-jdk openvpn git tmux nmap linux-headers-$(uname -r)
 
 # pip packages
 pip3 install requests
 
 # gdb peda extensions
+rm -rf /home/vagrant/peda
+rm -rf /home/vagrant/.gdbinit
 git clone https://github.com/longld/peda.git /home/vagrant/peda
-echo "source /home/peda/peda.py" >> /home/vagrant/.gdbinit
+echo "source /home/vagrant/peda/peda.py" >> /home/vagrant/.gdbinit
 chown vagrant:vagrant /home/vagrant/.gdbinit
 chown -R vagrant:vagrant /home/vagrant/peda
