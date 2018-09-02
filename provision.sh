9d843d2d5067e48ae6e587768cf9f3bf3d5d69b8
Isaac Sears <isaac.j.sears@gmail.com>
2018-09-02 13:49:15 -0400

    Fix peda installation

--- provision.sh
+++ provision.sh
@@ -4,12 +4,13 @@
 apt-get update
 apt-get -y upgrade
 apt-get -y install vim build-essential gcc-multilib g++-multilib gdb python3-pip \
-    default-jdk openvpn linux-headers-$(uname -r)
+    default-jdk openvpn git tmux linux-headers-$(uname -r)
 
 # pip packages
 pip3 install requests
 
 # gdb peda extensions
-git clone https://github.com/longld/peda.git /opt/peda
-echo "source ~/peda/peda.py" >> /home/vagrant/.gdbinit
+git clone https://github.com/longld/peda.git /home/vagrant/peda
+echo "source /home/peda/peda.py" >> /home/vagrant/.gdbinit
 chown vagrant:vagrant /home/vagrant/.gdbinit
+chown -R vagrant:vagrant /home/vagrant/peda
