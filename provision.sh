e26349a875fee09fb3d430281c46ed85b714faea
Isaac Sears <isaac.j.sears@gmail.com>
2018-09-02 13:45:56 -0400

    Install more tools

--- provision.sh
+++ provision.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-echo 'bereit' > /tmp/bereit.txt
+# apt packages
+apt-get update
+apt-get -y upgrade
+apt-get -y install vim build-essential gcc-multilib g++-multilib gdb python3-pip \
+    default-jdk openvpn linux-headers-$(uname -r)
+
+# pip packages
+pip3 install requests
+
+# gdb peda extensions
+git clone https://github.com/longld/peda.git /opt/peda
+echo "source ~/peda/peda.py" >> /home/vagrant/.gdbinit
+chown vagrant:vagrant /home/vagrant/.gdbinit
