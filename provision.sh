82c9edd53a8e28825fe09acba9a0cd74ea993bab
Isaac Sears <isaac.j.sears@gmail.com>
2018-09-02 16:00:56 -0400

    Install golang

--- provision.sh
+++ provision.sh
@@ -4,7 +4,7 @@
 apt-get update
 apt-get -y upgrade
 apt-get -y install vim build-essential gcc-multilib g++-multilib gdb python3-pip \
-    default-jdk openvpn git tmux nmap linux-headers-$(uname -r)
+    default-jdk openvpn git tmux nmap curl linux-headers-$(uname -r)
 
 # pip packages
 pip3 install requests
@@ -16,6 +16,25 @@ git clone https://github.com/longld/peda.git /home/vagrant/peda
 echo "source /home/vagrant/peda/peda.py" >> /home/vagrant/.gdbinit
 chown vagrant:vagrant /home/vagrant/.gdbinit
 chown -R vagrant:vagrant /home/vagrant/peda
+
+# go programming language
+curl -O https://dl.google.com/go/go1.11.linux-amd64.tar.gz
+echo "b3fcf280ff86558e0559e185b601c9eade0fd24c900b4c63cd14d1d38613e499 go1.11.linux-amd64.tar.gz" | sha256sum -c
+
+if [ $? == 0 ]; then
+    tar -xvf go1.11.linux-amd64.tar.gz
+    rm -rf /usr/local/go
+    mv go /usr/local/
+
+    bashrc_lastline=`tail -n 1 /home/vagrant/.bashrc`
+
+    if [ "$bashrc_lastline" != "export PATH=$PATH:/usr/local/go/bin" ]; then
+        echo "export PATH=$PATH:/usr/local/go/bin" >> /home/vagrant/.bashrc
+    fi
+
+else
+    echo "WARNING: Bad checksum for go, skipping installation"
+fi
 
 # rename
 echo "donthackmebro" > /etc/hostname
